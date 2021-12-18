e57ddd1483b6c55f8c51b48d5f1db7ba29285f57
skhin <88125976+skhin@users.noreply.github.com>
2021-12-18 18:23:35 +0800

    Created new React App with simple start page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="logo img-fluid" alt="logo" />
+        CHECK YOUR WORDS
+      </header>
+    </div>
+  );
+}
+
+export default App;
